51f2ce5b79dbdf2ea7b9d18aed132bc0fa517493
python <rahf123memo@gmail.com>
2026-03-17 20:33:45 +0300

    first commits1

--- Homework1.py
+++ Homework1.py
@@ -1,1 +1,12 @@
-print("hello world")
+students = []
+students.append(["Ali", 75])
+students.append(["Sara", 88])
+students.append(["Omar", 45])
+students.insert(2, ["Lina", 90])
+students[2][0] = "Mona"
+passed_students = []
+for s in students:
+    if s[1] >= 50:
+        passed_students.append(s)
+print("Students List:", students)
+print("Passed Students:", passed_students)
